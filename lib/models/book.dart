f5f8361122a8636bd98d4cdca8df438c9c70a753
girioaugusto <girioaugusto@gmail.com>
2025-09-04 17:06:21 -0300

    feat: adicionar estrutura inicial do projeto BookApp

---
+++ lib/models/book.dart
@@ -0,0 +1,66 @@
+class Book {
+  final String id;               // identificador único do volume
+  final String title;            // título do livro
+  final List<String> authors;    // lista de autores
+  final String? thumbnail;       // URL da imagem (pode não existir)
+  final double? price;           // preço (pode não existir)
+  final String? currency;        // moeda (ex.: USD, BRL)
+
+  Book({
+    required this.id,
+    required this.title,
+    required this.authors,
+    this.thumbnail,
+    this.price,
+    this.currency,
+  });
+
+  // Constrói a partir do item retornado pela Google Books API
+  factory Book.fromGoogleItem(Map<String, dynamic> json) {
+    final volumeInfo = json['volumeInfo'] ?? {};
+    final saleInfo = json['saleInfo'] ?? {};
+    final imageLinks = volumeInfo['imageLinks'] ?? {};
+
+    double? parsedPrice;
+    String? currency;
+
+    // Às vezes vem como listPrice, outras como retailPrice:
+    if (saleInfo['listPrice'] != null) {
+      parsedPrice = (saleInfo['listPrice']['amount'] as num?)?.toDouble();
+      currency = saleInfo['listPrice']['currencyCode'];
+    } else if (saleInfo['retailPrice'] != null) {
+      parsedPrice = (saleInfo['retailPrice']['amount'] as num?)?.toDouble();
+      currency = saleInfo['retailPrice']['currencyCode'];
+    }
+
+    return Book(
+      id: (json['id'] ?? '').toString(),
+      title: (volumeInfo['title'] ?? '').toString(),
+      authors: (volumeInfo['authors'] as List?)
+              ?.map((e) => e.toString())
+              .toList() ?? <String>[],
+      thumbnail: (imageLinks['thumbnail'] ?? imageLinks['smallThumbnail'])?.toString(),
+      price: parsedPrice,
+      currency: currency,
+    );
+  }
+
+  // Para persistir favorito localmente
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'authors': authors,
+    'thumbnail': thumbnail,
+    'price': price,
+    'currency': currency,
+  };
+
+  factory Book.fromJson(Map<String, dynamic> json) => Book(
+    id: (json['id'] ?? '').toString(),
+    title: (json['title'] ?? '').toString(),
+    authors: (json['authors'] as List?)?.map((e) => e.toString()).toList() ?? <String>[],
+    thumbnail: json['thumbnail'] as String?,
+    price: (json['price'] as num?)?.toDouble(),
+    currency: json['currency'] as String?,
+  );
+}
